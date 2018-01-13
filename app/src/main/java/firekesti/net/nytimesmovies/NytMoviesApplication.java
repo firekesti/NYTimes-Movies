f3591102b3265268e4f4e51372b681bd54a38c05
Kate Kelly <firekesti@users.noreply.github.com>
2018-01-13 14:22:25 -0500

    add leak detection (just in case LiveData isn't set up properly)

--- app/src/main/java/firekesti/net/nytimesmovies/NytMoviesApplication.java
+++ app/src/main/java/firekesti/net/nytimesmovies/NytMoviesApplication.java
@@ -2,6 +2,8 @@ package firekesti.net.nytimesmovies;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import firekesti.net.nytimesmovies.mylist.MyListStore;
 
 /**
@@ -11,6 +13,12 @@ public class NytMoviesApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
         MyListStore.init(this);
     }
 }
